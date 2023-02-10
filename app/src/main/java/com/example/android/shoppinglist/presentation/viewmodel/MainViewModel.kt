780873a7e632cfa02fceef8b300b2ccbe2f80082
vpetel127 <vpetel127@cable.comcast.com>
2023-02-10 08:28:59 -0500

    Working database

--- app/src/main/java/com/example/android/shoppinglist/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/android/shoppinglist/presentation/viewmodel/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.android.shoppinglist.presentation.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.example.android.shoppinglist.data.ShopListRepositoryImpl
 import com.example.android.shoppinglist.domain.usecase.DeleteShopItemUseCase
@@ -7,8 +9,8 @@ import com.example.android.shoppinglist.domain.usecase.EditShopItemUseCase
 import com.example.android.shoppinglist.domain.usecase.GetShopListUseCase
 import com.example.android.shoppinglist.domain.pojo.ShopItem
 
-class MainViewModel : ViewModel() {
-    private val repository = ShopListRepositoryImpl
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = ShopListRepositoryImpl(application)
 
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
--- app/src/main/java/com/example/android/shoppinglist/presentation/viewmodel/ShopItemViewModel.kt
+++ app/src/main/java/com/example/android/shoppinglist/presentation/viewmodel/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.android.shoppinglist.presentation.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,8 +11,8 @@ import com.example.android.shoppinglist.domain.usecase.EditShopItemUseCase
 import com.example.android.shoppinglist.domain.usecase.GetShopItemUseCase
 import com.example.android.shoppinglist.domain.pojo.ShopItem
 
-class ShopItemViewModel : ViewModel() {
-    private val repository = ShopListRepositoryImpl
+class ShopItemViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = ShopListRepositoryImpl(application)
 
     private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
